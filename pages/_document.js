6aab73cb4b6217a3b0e016557a9476d12cb618c0
renaldisigit <renaldisigitpratama87@gmail.com>
2022-06-28 14:09:15 +0700

    upload all files

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <div>
+          <meta charSet="UTF-8" />
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="width=device-width,initial-scale=1" name="viewport" />
+          <meta content="description" name="description" />
+          <meta name="google" content="notranslate" />
+          <meta
+            content="Mashup templates have been developped by Orson.io team"
+            name="author"
+          />
+          {/* Disable tap highlight on IE */}
+          <meta name="msapplication-tap-highlight" content="no" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="./assets/apple-icon-180x180.png"
+          />
+          <link href="./assets/favicon.ico" rel="icon" />
+          <title>Renaldi Sigit Pratama</title>
+          <link href="./main.d8e0d294.css" rel="stylesheet" />
+        </div>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+
+        <script type="text/javascript" src="./main.bc58148c.js"></script>
+      </body>
+    </Html>
+  );
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import fs from "fs";
+import path from "path";
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const directory = path.join(__dirname + "../../../../../database");
+  const Homejson = fs.readFileSync(`${directory}/home.json`, {
+    encoding: "utf-8",
+  });
+  res.status(200).json({ Homejson: JSON.parse(Homejson) });
 }
